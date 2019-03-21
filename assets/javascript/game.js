4fcfbf8e76fd7409b051d494e47cdf9fd2bf4f58
M.Zarli <16181950+sharukurusu@users.noreply.github.com>
2019-03-21 18:16:03 -0400

    added reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,34 @@
-// Select word randomly from pool, without repeating word
-// Display blank lines equal to word length
-// Take user input letter
-// If user input matches letter in word, display in correct spot or spots
-// If letter does not match, lose one guess, display in list of wrong chances
-// If word is completed, show image and play sound
-// If out of chances, show alternate image and play sound
-// Reset board with new word
-// If out of words display big win message and reset play
+// Contents:
+// -vars
+// -initial setup
+// -game logic
+// -functions
+
 var score = 0,
-chances = 10,
-usedLetters = [],
-wordPool = ["avast", "ahoy", "matey", "scurvy", "eyepatch", "parrot", "treasure", "booty", "cutlass", "blunderbuss", "buccaneer", "cannon", "arrrrrrrr"],
+    chances = 10,
+    usedLetters = [],
+    wordPool = ["matey", "scurvy", "eyepatch", "parrot", "treasure", "booty", "cutlass", "buccaneer", "cannon", "arrrrrrrr"],
+    currentWord = "",
+    playArea = [];
 
-// Selects word at random from wordPool
-currentWord = wordPool[Math.floor(Math.random()*wordPool.length)];
-// console.log(currentWord)
-var blankLength = currentWord.length,
-
-// Creates playArea array and fills with placeholder underscores
-playArea = [];
-for (i = 0; i < blankLength; i++) {
-    playArea.push("_")
+// Initial setup
+shuffle(wordPool)
+function nextWord() {
+    resetPlayVars()
+    makeBlanks()
+    resetDisplay()
 }
+nextWord()
 
-// Displays initial playArea
-document.getElementById('playArea').textContent = playArea.join(" ");
-document.getElementById('chances').textContent = chances;
-
+// Runs game logic when key is pressed
 document.onkeyup = function(event) {
     var userInput = event.key.toLowerCase(),
         matches = [];
 
-        // Checks if the userInput string length is 1 to exclude function keys & checks if it is only a letter not a number
+    // Checks if the userInput string length is 1 to exclude function keys & checks if it is only a letter not a number
     if (userInput.length === 1 && userInput.search(/[a-z]/) > -1 ) {
         // Checks if the letter has been used already
-        if (usedLetters.includes(userInput)) {
+        if (window.usedLetters.includes(userInput)) {
             // If currentWord does not contain userInput, player loses a chance, and it displays the letter in the wrongGuesses div
         } else if (currentWord.indexOf(userInput) === -1) {
             chances--
@@ -42,9 +36,9 @@ document.onkeyup = function(event) {
             document.getElementById('wrongGuesses').textContent += userInput.toUpperCase() + " ";
             // loops over the current word and if the userInput occurs, it stores the index in a matches array
         } else {
-            for (var i = 0; i < blankLength; i++) {
-                if (currentWord[i] === userInput) {
-                    matches.push(i);
+            for (var v = 0; v < blankLength; v++) {
+                if (currentWord[v] === userInput) {
+                    matches.push(v);
                 }
             }
         }
@@ -52,41 +46,77 @@ document.onkeyup = function(event) {
         usedLetters.push(userInput)
     }
     // Uses match indices to replace same indices in playArea array with userInput
-    for (i = 0; i < matches.length; i++) {
-        playArea[matches[i]] = userInput
+    for (b = 0; b < matches.length; b++) {
+        playArea[matches[b]] = userInput
     }
 
-// Displays contents of playArea with commas replaced by spaces for readability
-    document.getElementById('playArea').textContent = playArea.join(" ").toLocaleUpperCase();
+    // Displays contents of playArea with commas replaced by spaces for readability
+    document.getElementById('playArea').textContent = playArea.join(" ").toUpperCase();
     
     // game over if chances reach zero
     if (chances <= 0) {
-        document.getElementById('playArea').textContent = "You lose!"
-    }
-
-// Detects if word is complete and resets play for next word
-    if (playArea.join("").toString() === currentWord) {
+        document.getElementById('title').textContent = "You Lose!"
+        score = 0
+        shuffle(wordPool);
+        setTimeout( function(){
+            document.getElementById('title').textContent = "Pirate's Hangman!"
+            nextWord()
+        }, 3000)
+    // Detects if word is complete and resets play for next word
+    } else if (playArea.join("").toString() === currentWord) {
         score++
-        currentWord = wordPool[Math.floor(Math.random()*wordPool.length)];
-        chances = 10,
-        usedLetters = []
-        playArea = [],
-        blankLength = currentWord.length;
-        for (i = 0; i < blankLength; i++) {
-            playArea.push("_")
+        if (score < wordPool.length) {
+            nextWord()
+            // if score === word pool, player wins, game resets
+        }   else if (score === wordPool.length) {
+            document.getElementById('title').textContent = "You Win!"
+            shuffle(wordPool);
+            score = 0
+            setTimeout( function(){
+                document.getElementById('title').textContent = "Pirate's Hangman!"
+                nextWord()
+            }, 3000)
         }
-        document.getElementById('wrongGuesses').textContent = null
-        document.getElementById('score').textContent = score
-        document.getElementById('playArea').textContent = playArea.join(" ")
-        document.getElementById('chances').textContent = chances
+
     }
 }
 
+// Fisher-Yates Shuffle to randomize the wordPool
+function shuffle(array) {
+    var j, x, i;
+    for (i = array.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = array[i];
+        array[i] = array[j];
+        array[j] = x;
+    }
+    return array
+};
 
+// Function to be called when new word to guess is needed
+function resetPlayVars() {
+    currentWord = wordPool[score]
+    chances = 10
+    usedLetters = []
+    playArea = []
+};
 
+// Fills playArea array with placeholder underscores
+function makeBlanks() {
+    blankLength = currentWord.length;
+    for (c = 0; c < blankLength; c++) {
+        playArea.push("_");
+    }
+}
 
-
-
+// Resets the display
+function resetDisplay() {
+    document.getElementById('wrongGuesses').textContent = null
+    document.getElementById('score').textContent = score
+    document.getElementById('playArea').textContent = playArea.join(" ")
+    document.getElementById('chances').textContent = chances
+}
+   
 
 
 
